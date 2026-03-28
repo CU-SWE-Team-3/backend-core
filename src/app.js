4b9694206c73ea3b9cf78cf29154f9f889c4ad95
Mohamed Hatem <mohammed.mahmoud05@eng-st.cu.edu.eg>
2026-03-28 05:30:21 +0200

    feat(module5): implement user playback history, accessibility limits, and the 90% playcount rule

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const networkRoutes = require('./routes/networkRoutes');
 
+const historyRouter = require('./routes/historyRoutes');
+
 const trackRoutes = require('./routes/trackRoutes');
 const authRoutes = require('./routes/authRoutes');
 const profileRoutes = require('./routes/profileRoutes');
@@ -89,6 +91,7 @@ app.use(mongoSanitize());
 // 2. ROUTES
 // ==========================================
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
+app.use('/api/history', historyRouter);
 
 app.get('/', (req, res) => {
   res.status(200).json({
